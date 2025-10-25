696829922b5df249b4ab76f494863a118ec64221
Basia Stolarz <bstolarz@gmail.com>
2025-10-25 21:23:18 +0200

    feat: add Magazine WIP and selection

--- assignment-1-library/src/main/java/com/codefirstgirls/Main.java
+++ assignment-1-library/src/main/java/com/codefirstgirls/Main.java
@@ -7,11 +7,14 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in); // Scanner class is used to get user input
 
     public static void main(String[] args) {
-        System.out.println("WELCOME TO THE OBJECT GENERATOR");
+        System.out.println("WELCOME TO THE LIBRARY ITEM GENERATOR");
         System.out.println("*******************************");
         System.out.println("PLEASE ENTER ONE OF THE FOLLOWING OBJECTS TO CREATE");
 
+
         //print out your list here
+        System.out.println("1. BOOK");
+        System.out.println("2. MAGAZINE (WIP)");
 
         String menuChoice = scanner.nextLine();
         System.out.println("THANKS! HOW MANY WOULD YOU LIKE TO CREATE?");
